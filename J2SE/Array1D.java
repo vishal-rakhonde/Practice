4fb08763887cfc9ef8505983f6c48f3bad622894
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-04-16 22:45:15 +0530

    Add files via upload

---
+++ J2SE/Array1D.java
@@ -0,0 +1,11 @@
+public class Array1D {
+
+	public static void main(String[] args) {
+		int a[]=new int[] {10,20,30,40,50};
+		for(int i=0;i<a.length;i++) {
+			System.out.print(a[i]+" ");
+		}
+
+	}
+
+}
