66697f1b57062b47fd5e3fda4e8da082ca7aa285
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-04-16 22:40:55 +0530

    Add files via upload

---
+++ J2SE/Alphabates.java
@@ -0,0 +1,16 @@
+public class Alphabates {
+
+	public static void main(String[] args) {
+		char a='A';
+		char b='a';
+		for(int i=1;i<=26;i++) {
+			System.out.print(a+" ");
+			a++;
+		}System.out.println();
+		for(int i=1;i<=26;i++) {
+			System.out.print(b+" ");
+			b++;
+		}
+	}
+
+}
