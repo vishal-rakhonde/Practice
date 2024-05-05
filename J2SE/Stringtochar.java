4d518cb2074c4c3025c75975600633c3fb35c572
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-05-05 09:38:35 +0530

    Add files via upload

---
+++ J2SE/Stringtochar.java
@@ -0,0 +1,11 @@
+
+class StringToChar{
+	public static void main(String [] args){
+	String s="hello";
+	
+	for(int i=0;i<s.length();i++){
+		char ch=s.charAt(i);
+		System.out.print(ch+" ");
+		}
+	}
+}
