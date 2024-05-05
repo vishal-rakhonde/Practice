b64df26bcad809e298ff3be85f2d393e1a2b014a
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-05-05 23:45:06 +0530

    Add files via upload

---
+++ J2SE/Pyramid.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pyramid{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of rows of Pyramid");
+		int n = sc.nextInt(); 
+		for (int i=0; i<n; i++){
+			for (int j=n-i; j>1; j--){
+				System.out.print(" ");
+			}
+			for (int j=0; j<=i; j++ ){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
