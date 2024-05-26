eef542ca49e384277952dbd315307429e4de4b84
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-05-27 01:24:19 +0530

    Add files via upload

---
+++ J2SE/Pattern/DecendingOrderPattern.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class DecendingOrderPattern{
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+         int rows =sc.nextInt();
+        for (int i = 1; i <= rows; i++) { 
+            for (int j = rows; j >= i; j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
