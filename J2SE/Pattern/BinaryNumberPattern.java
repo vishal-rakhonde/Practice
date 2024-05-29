ab36fefe8c040ceafab5454bf17c813530a03d93
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-05-29 12:40:48 +0530

    Add files via upload

---
+++ J2SE/Pattern/BinaryNumberPattern.java
@@ -0,0 +1,16 @@
+//The Java code for printing a binary number pattern
+
+public class BinaryNumberPattern {
+    public static void main(String[] args) {
+        int n=5;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if(j%2==0) System.out.print(0);
+                else System.out.print(1);
+            }
+
+            System.out.println();
+        }
+    }
+}
