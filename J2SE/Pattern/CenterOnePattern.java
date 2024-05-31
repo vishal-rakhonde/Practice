667b468c6e4b1ba8422459c6cf4d010ce08ac07b
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-06-01 01:40:52 +0530

    Add files via upload

---
+++ J2SE/Pattern/CenterOnePattern.java
@@ -0,0 +1,20 @@
+
+//The Java code for printing a ones pattern .
+
+public class CenterOnePattern{
+
+    public static void main(String[] args) {
+         
+        int n = 5; 
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=(n-i); j++) {
+                System.out.print(" ");
+            }
+            for(int k=1; k<=(2*i)-1; k++) {
+                System.out.print("1");
+            }
+            System.out.println();
+        }
+    }
+
+}
