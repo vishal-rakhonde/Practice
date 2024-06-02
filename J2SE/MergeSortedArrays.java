fd961b40bae738a78ca941f709c69166b4f7e1b3
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-06-03 01:23:13 +0530

    Add files via upload

---
+++ J2SE/MergeSortedArrays.java
@@ -0,0 +1,38 @@
+//How to merge two sorted arrays in Java?
+
+public class MergeSortedArrays {
+    public static int[] mergeArrays(int[] arr1, int[] arr2) {
+        int[] mergedArray = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                mergedArray[k++] = arr1[i++];
+            } else {
+                mergedArray[k++] = arr2[j++];
+            }
+        }
+
+        while (i < arr1.length) {
+            mergedArray[k++] = arr1[i++];
+        }
+
+        while (j < arr2.length) {
+            mergedArray[k++] = arr2[j++];
+        }
+
+        return mergedArray;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7, 9};
+        int[] arr2 = {2, 4, 6, 8, 10};
+
+        int[] mergedArray = mergeArrays(arr1, arr2);
+
+        System.out.println("Merged Array:");
+        for (int num : mergedArray) {
+            System.out.print(num + " ");
+        }
+    }
+}
