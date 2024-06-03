bb7316e2d9398d30d048fcc76ce1abcda446df2f
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-06-04 00:25:45 +0530

    Add files via upload

---
+++ J2SE/Pattern/DimondPattern.java
@@ -0,0 +1,30 @@
+
+// The Java code for printing a diamond shape pattern is given below:
+
+
+ public class DimondPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n - i; j++)
+                System.out.print(" ");
+
+            for (int j = 0; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+
+        for (int i = n; i >0; i--) {
+
+            for (int j = 0; j <= n - i; j++)
+                System.out.print(" ");
+
+            for (int j = 0; j < i; j++)
+                System.out.print("* ");
+
+            System.out.println();
+        }
+    }
+}
