2e8e0a2c740431a3812776ef2b70419fac76643f
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-06-23 20:15:39 +0530

    Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

--- J2SE/LongestSubarray.java
+++ J2SE/LongestSubarray.java
@@ -1,8 +1,5 @@
 /* Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
-Medium
-Topics
-Companies
-Hint
+
 Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
 
 Example 1:
@@ -35,3 +32,44 @@ Constraints:
 1 <= nums[i] <= 109
 0 <= limit <= 109
 */
+
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+        
+        LinkedList<Integer> increase = new LinkedList<>();
+        LinkedList<Integer> decrease = new LinkedList<>();
+
+        int max = 0;
+        int left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+
+            while (increase.size() > 0 && n < increase.getLast()) {
+                increase.removeLast();
+            }
+            increase.add(n);
+
+            while (decrease.size() > 0 && n > decrease.getLast()) {
+                decrease.removeLast();
+            }
+
+            decrease.add(n);
+
+            while (decrease.getFirst() - increase.getFirst() > limit) {
+                if (nums[left] == decrease.getFirst()) {
+                    decrease.removeFirst();
+                }
+                if (nums[left] == increase.getFirst()) {
+                    increase.removeFirst();
+                }
+                left++;
+            }
+
+            int size = i - left + 1;
+            max = Math.max(max, size);
+        }
+
+        return max;
+    }
+}
