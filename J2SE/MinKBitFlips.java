f11380357e45e1d661a3d2f422dde3b6284af943
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-06-24 23:28:08 +0530

    Minimum Number of K Consecutive Bit Flips

--- J2SE/MinKBitFlips.java
+++ J2SE/MinKBitFlips.java
@@ -34,3 +34,21 @@ Constraints:
 1 <= nums.length <= 105
 1 <= k <= nums.length
 */
+class MinKBitFlips {
+    public int minKBitFlips(int[] nums, int k) {
+        int n = nums.length, flipped = 0, res = 0;
+        int[] isFlipped = new int[n];
+        for (int i = 0; i < nums.length; ++i) {
+            if (i >= k)
+                flipped ^= isFlipped[i - k];
+                if (flipped == nums[i]) {
+                    if (i + k > nums.length)
+                        return -1;
+                isFlipped[i] = 1;
+                flipped ^= 1;
+                res++;
+            }
+        }
+        return res;
+    }
+}
