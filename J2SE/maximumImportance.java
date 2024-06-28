58ed27fbd74b81e4258730b06b4991a57acc17b4
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-06-28 23:27:45 +0530

    Update maximumImportance.java

--- J2SE/maximumImportance.java
+++ J2SE/maximumImportance.java
@@ -20,9 +20,8 @@ Explanation: The figure above shows the country and the assigned values of [2,4,
 - The road (2,4) has an importance of 5 + 1 = 6.
 The total importance of all roads is 6 + 9 + 8 + 7 + 7 + 6 = 43.
 It can be shown that we cannot obtain a greater total importance than 43.
+
 Example 2:
-
-
 Input: n = 5, roads = [[0,3],[2,4],[1,3]]
 Output: 20
 Explanation: The figure above shows the country and the assigned values of [4,3,2,5,1].
@@ -31,10 +30,8 @@ Explanation: The figure above shows the country and the assigned values of [4,3,
 - The road (1,3) has an importance of 3 + 5 = 8.
 The total importance of all roads is 9 + 3 + 8 = 20.
 It can be shown that we cannot obtain a greater total importance than 20.
- 
 
 Constraints:
-
 2 <= n <= 5 * 104
 1 <= roads.length <= 5 * 104
 roads[i].length == 2
