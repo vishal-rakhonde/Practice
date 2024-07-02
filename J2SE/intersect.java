94adcfa36b25005e673550d14b914e387c0204b1
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-07-02 22:58:06 +0530

    Update intersect.java

--- J2SE/intersect.java
+++ J2SE/intersect.java
@@ -25,3 +25,33 @@ What if the given array is already sorted? How would you optimize your algorithm
 What if nums1's size is small compared to nums2's size? Which algorithm is better?
 What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 */
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        int i = 0, j = 0;
+        List<Integer> intersection = new ArrayList<>();
+        
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                intersection.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        
+        // Convert the list to an array
+        int[] result = new int[intersection.size()];
+        for (int k = 0; k < intersection.size(); k++) {
+            result[k] = intersection.get(k);
+        }
+        
+        return result;
+    }
+}
