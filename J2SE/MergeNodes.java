f158c5f8779cbfd5262c92136400a97f977635ad
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-07-04 23:48:04 +0530

    Merge Nodes in Between Zeros

--- J2SE/MergeNodes.java
+++ J2SE/MergeNodes.java
@@ -6,20 +6,15 @@ For every two consecutive 0's, merge all the nodes lying in between them into a 
 
 Return the head of the modified linked list.
 
- 
-
 Example 1:
-
-
 Input: head = [0,3,1,0,4,5,2,0]
 Output: [4,11]
 Explanation: 
 The above figure represents the given linked list. The modified list contains
 - The sum of the nodes marked in green: 3 + 1 = 4.
 - The sum of the nodes marked in red: 4 + 5 + 2 = 11.
+
 Example 2:
-
-
 Input: head = [0,1,0,3,0,2,2,0]
 Output: [1,3,4]
 Explanation: 
@@ -28,11 +23,10 @@ The above figure represents the given linked list. The modified list contains
 - The sum of the nodes marked in red: 3 = 3.
 - The sum of the nodes marked in yellow: 2 + 2 = 4.
  
-
 Constraints:
-
 The number of nodes in the list is in the range [3, 2 * 105].
 0 <= Node.val <= 1000
 There are no two consecutive nodes with Node.val == 0.
 The beginning and end of the linked list have Node.val == 0
 */
+
