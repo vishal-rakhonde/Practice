a5526331039d5c2f5ada9783a78ee4874d56bfff
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-07-07 23:20:05 +0530

    Update numWaterBottles.java

--- J2SE/numWaterBottles.java
+++ J2SE/numWaterBottles.java
@@ -6,11 +6,7 @@ The operation of drinking a full water bottle turns it into an empty bottle.
 
 Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
 
- 
-
 Example 1:
-
-
 Input: numBottles = 9, numExchange = 3
 Output: 13
 Explanation: You can exchange 3 empty bottles to get 1 full water bottle.
@@ -29,3 +25,20 @@ Constraints:
 1 <= numBottles <= 100
 2 <= numExchange <= 100
 */
+
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+
+        int count = numBottles;
+
+        while(numBottles >= numExchange){
+            count+=numBottles/numExchange;
+            int Bottles =numBottles %numExchange;
+
+            numBottles = (numBottles/numExchange)+Bottles;
+        }
+
+        return count;
+        
+    }
+}
