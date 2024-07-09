1fb7b23df0941678c6420997d23fb77cd0aa371e
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-07-09 23:59:19 +0530

    Update averageWaitingTime.java

--- J2SE/averageWaitingTime.java
+++ J2SE/averageWaitingTime.java
@@ -1,8 +1,5 @@
 /* Average Waiting Time
-Medium
-Topics
-Companies
-Hint
+
 There is a restaurant with a single chef. You are given an array customers, where customers[i] = [arrivali, timei]:
 
 arrivali is the arrival time of the ith customer. The arrival times are sorted in non-decreasing order.
@@ -11,10 +8,7 @@ When a customer arrives, he gives the chef his order, and the chef starts prepar
 
 Return the average waiting time of all customers. Solutions within 10-5 from the actual answer are considered accepted.
 
- 
-
 Example 1:
-
 Input: customers = [[1,2],[2,5],[4,3]]
 Output: 5.00000
 Explanation:
@@ -22,8 +16,8 @@ Explanation:
 2) The second customer arrives at time 2, the chef takes his order and starts preparing it at time 3, and finishes at time 8, so the waiting time of the second customer is 8 - 2 = 6.
 3) The third customer arrives at time 4, the chef takes his order and starts preparing it at time 8, and finishes at time 11, so the waiting time of the third customer is 11 - 4 = 7.
 So the average waiting time = (2 + 6 + 7) / 3 = 5.
-Example 2:
 
+Example 2:
 Input: customers = [[5,2],[5,4],[10,3],[20,1]]
 Output: 3.25000
 Explanation:
@@ -35,7 +29,6 @@ So the average waiting time = (2 + 6 + 4 + 1) / 4 = 3.25.
  
 
 Constraints:
-
 1 <= customers.length <= 105
 1 <= arrivali, timei <= 104
 arrivali <= arrivali+1
