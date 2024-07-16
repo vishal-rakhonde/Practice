33e22e9fc67c2235c1ab7d9d3fbf63ba201a493f
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-07-17 00:08:02 +0530

    LeetCode

--- ADS/BinaryTreePathFinder.java
+++ ADS/BinaryTreePathFinder.java
@@ -31,3 +31,19 @@ All the values in the tree are unique.
 1 <= startValue, destValue <= n
 startValue != destValue
 */
+
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
