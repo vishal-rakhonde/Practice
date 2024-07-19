6357355562a36f298e2a0d254ac65b0fafe383ed
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-07-20 00:33:46 +0530

    code

--- J2SE/LuckyNumberFinder.java
+++ J2SE/LuckyNumberFinder.java
@@ -29,3 +29,53 @@ n == mat[i].length
 1 <= matrix[i][j] <= 105.
 All elements in the matrix are distinct.
 */
+import java.util.ArrayList;
+import java.util.List;
+
+class LuckyNumberFinder {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        int N = matrix.length, M = matrix[0].length;
+
+        List<Integer> rowMin = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            int rMin = Integer.MAX_VALUE;
+            for (int j = 0; j < M; j++) {
+                rMin = Math.min(rMin, matrix[i][j]);
+            }
+            rowMin.add(rMin);
+        }
+
+        List<Integer> colMax = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            int cMax = Integer.MIN_VALUE;
+            for (int j = 0; j < N; j++) {
+                cMax = Math.max(cMax, matrix[j][i]);
+            }
+            colMax.add(cMax);
+        }
+
+        List<Integer> luckyNumbers = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (matrix[i][j] == rowMin.get(i) && matrix[i][j] == colMax.get(j)) {
+                    luckyNumbers.add(matrix[i][j]);
+                }
+            }
+        }
+
+        return luckyNumbers;
+    }
+
+    public static void main(String[] args) {
+        LuckyNumberFinder finder = new LuckyNumberFinder();
+        
+        int[][] matrix = {
+            {3, 7, 8},
+            {9, 11, 13},
+            {15, 16, 17}
+        };
+        
+        List<Integer> result = finder.luckyNumbers(matrix);
+        System.out.println(result); // Output: [15]
+    }
+}
