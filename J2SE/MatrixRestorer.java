347e923331c0025037a8e93ef0dc120888772824
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-07-21 00:02:11 +0530

    upload

--- J2SE/MatrixRestorer.java
+++ J2SE/MatrixRestorer.java
@@ -35,3 +35,43 @@ Constraints:
 sum(rowSum) == sum(colSum)
 
 */
+
+public class MatrixRestorer {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int numrows = rowSum.length;
+        int colrows = colSum.length;
+        int[][] result = new int[numrows][colrows];
+
+        int i = 0, j = 0;
+
+        while (i < numrows && j < colrows) {
+            int val = Math.min(rowSum[i], colSum[j]);
+            result[i][j] = val;
+            rowSum[i] -= val;
+            colSum[j] -= val;
+
+            if (rowSum[i] == 0) {
+                i++;
+            }
+            if (colSum[j] == 0) {
+                j++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MatrixRestorer solution = new MatrixRestorer();
+        int[] rowSum = {3, 8};
+        int[] colSum = {4, 7};
+        int[][] matrix = solution.restoreMatrix(rowSum, colSum);
+
+        // Print the result matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
