17ea5cff9a7e47e04648171e93ab90fbbf162e12
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-07-27 00:51:59 +0530

    program

--- J2SE/FindCityWithFewestNeighbors.java
+++ J2SE/FindCityWithFewestNeighbors.java
@@ -45,3 +45,57 @@ edges[i].length == 3
 1 <= weighti, distanceThreshold <= 10^4
 All pairs (fromi, toi) are distinct.
 */
+
+import java.util.Arrays;
+
+public class FindCityWithFewestNeighbors {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] dist = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], 10001);
+            dist[i][i] = 0;
+        }
+        
+        for (int[] edge : edges) {
+            dist[edge[0]][edge[1]] = edge[2];
+            dist[edge[1]][edge[0]] = edge[2];
+        }
+        
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        
+        int minReachableCities = n;
+        int result = -1;
+        
+        for (int i = 0; i < n; i++) {
+            int reachableCities = 0;
+            for (int j = 0; j < n; j++) {
+                if (dist[i][j] <= distanceThreshold) {
+                    reachableCities++;
+                }
+            }
+            if (reachableCities <= minReachableCities) {
+                minReachableCities = reachableCities;
+                result = i;
+            }
+        }
+        
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        FindCityWithFewestNeighbors solution = new FindCityWithFewestNeighbors();
+        int n = 4;
+        int[][] edges = { {0, 1, 3}, {1, 2, 1}, {1, 3, 4}, {2, 3, 1} };
+        int distanceThreshold = 4;
+        
+        int result = solution.findTheCity(n, edges, distanceThreshold);
+        System.out.println("The city with the smallest number of reachable cities is: " + result);
+    }
+}
