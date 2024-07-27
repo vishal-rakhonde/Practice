be8a2b23b2be8cf574eee2566025718c32fac6a6
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-07-28 02:42:19 +0530

    MinimumCostCalculator

--- J2SE/MinimumCostCalculator.java
+++ J2SE/MinimumCostCalculator.java
@@ -41,3 +41,53 @@ original[i], changed[i] are lowercase English letters.
 1 <= cost[i] <= 106
 original[i] != changed[i]
 */
+import java.util.Arrays;
+
+public class MinimumCostCalculator {
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        long arr[][] = new long[26][26];
+        for (int i = 0; i < 26; i++) {
+            Arrays.fill(arr[i], Integer.MAX_VALUE);
+            arr[i][i] = 0;
+        }
+        for (int i = 0; i < original.length; i++) {
+            int val1 = original[i] - 'a';
+            int val2 = changed[i] - 'a';
+            arr[val1][val2] = Math.min(arr[val1][val2], cost[i]);
+        }
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                for (int k = 0; k < 26; k++) {
+                    arr[j][k] = Math.min(arr[j][k], arr[j][i] + arr[i][k]);
+                }
+            }
+        }
+        long ans = 0;
+        for (int i = 0; i < source.length(); i++) {
+            int val1 = source.charAt(i) - 'a';
+            int val2 = target.charAt(i) - 'a';
+
+            if (val1 == val2) {
+                continue;
+            }
+
+            if (arr[val1][val2] == Integer.MAX_VALUE) {
+                return -1L;
+            } else {
+                ans += arr[val1][val2];
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        MinimumCostCalculator calculator = new MinimumCostCalculator();
+        String source = "abc";
+        String target = "bcd";
+        char[] original = {'a', 'b', 'c'};
+        char[] changed = {'b', 'c', 'd'};
+        int[] cost = {1, 1, 1};
+        long result = calculator.minimumCost(source, target, original, changed, cost);
+        System.out.println("Minimum cost: " + result);
+    }
+}
