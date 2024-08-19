396d1aa199964d267347f54ef224deec8ad5e6ed
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-08-19 21:50:49 +0530

    2 Keys Keyboard

--- LeetCode/CommitSteps.java
+++ LeetCode/CommitSteps.java
@@ -25,3 +25,29 @@ Constraints:
 
 1 <= n <= 1000
 */
+class CommitSteps {
+    private int targetLength;
+    
+    public int minSteps(int n) {
+        if (n == 1) return 0;
+        this.targetLength = n;
+        return 1 + findMinSteps(1, 1);
+    }
+    
+    private int findMinSteps(int currentLength, int clipboardLength) {
+        if (currentLength == targetLength) return 0;
+        if (currentLength > targetLength) return Integer.MAX_VALUE / 2;
+        
+        int copyAndPaste = 2 + findMinSteps(currentLength * 2, currentLength);
+        int pasteOnly = 1 + findMinSteps(currentLength + clipboardLength, clipboardLength);
+        
+        return Math.min(copyAndPaste, pasteOnly);
+    }
+    
+    public static void main(String[] args) {
+        CommitSteps solution = new CommitSteps();
+        int n = 5; 
+        int result = solution.minSteps(n);
+        System.out.println("Minimum steps to get " + n + " 'A's: " + result);
+    }
+}
