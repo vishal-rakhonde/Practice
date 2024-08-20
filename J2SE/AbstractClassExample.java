e705ba5b05b818251cae18111da0368f07b0ff85
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-08-20 06:04:14 +0530

    Abstract class

---
+++ J2SE/AbstractClassExample.java
@@ -0,0 +1,21 @@
+abstract class AbstractClassExample {
+	abstract void print();
+	public void display(){
+		System.out.println("In display method");
+	}
+}
+class Demo extends AbstractClassExample {
+	@Override 
+	void print(){
+		System.out.println("In print method");
+	}
+	public static void main(String[] args){
+		Demo d=new Demo();
+		d.print();
+		d.display();
+	}
+}
+
+// output :
+// In print method
+// In display method
