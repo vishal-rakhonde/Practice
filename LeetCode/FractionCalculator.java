87a94cf166d1999d6c304549bb79794196f41707
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-08-23 23:09:11 +0530

    leetcode

--- LeetCode/FractionCalculator.java
+++ LeetCode/FractionCalculator.java
@@ -30,3 +30,63 @@ The input only contains valid irreducible fractions, where the numerator and den
 The number of given fractions will be in the range [1, 10].
 The numerator and denominator of the final result are guaranteed to be valid and in the range of 32-bit int.
 */
+
+class FractionCalculator {
+    public String fractionAddition(String expression) {
+        int numerator = 0, denominator = 1;
+        int index = 0, n = expression.length();
+
+        while (index < n) {
+            // Read numerator
+            int numStart = index;
+            while (index < n && expression.charAt(index) != '/') {
+                index++;
+            }
+            int num = Integer.parseInt(expression.substring(numStart, index));
+
+            // Skip '/'
+            index++;
+
+            // Read denominator
+            int denStart = index;
+            while (index < n && Character.isDigit(expression.charAt(index))) {
+                index++;
+            }
+            int den = Integer.parseInt(expression.substring(denStart, index));
+
+            // Update the numerator and denominator
+            numerator = numerator * den + num * denominator;
+            denominator *= den;
+
+            // Simplify the fraction
+            int gcdVal = gcd(Math.abs(numerator), denominator);
+            numerator /= gcdVal;
+            denominator /= gcdVal;
+        }
+
+        return numerator + "/" + denominator;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        FractionCalculator calculator = new FractionCalculator();
+        
+        // Test cases
+        String expression1 = "-1/2+1/2";
+        String expression2 = "-1/2+1/2+1/3";
+        String expression3 = "1/3-1/2";
+
+        // Results
+        System.out.println("Result of \"" + expression1 + "\": " + calculator.fractionAddition(expression1)); // "0/1"
+        System.out.println("Result of \"" + expression2 + "\": " + calculator.fractionAddition(expression2)); // "1/3"
+        System.out.println("Result of \"" + expression3 + "\": " + calculator.fractionAddition(expression3)); // "-1/6"
+    }
+}
