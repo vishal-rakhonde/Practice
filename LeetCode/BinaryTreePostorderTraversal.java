bb94f6faa7a4acec76e44d0f9ce0d042145def6f
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-08-26 00:39:39 +0530

    post order

--- LeetCode/BinaryTreePostorderTraversal.java
+++ LeetCode/BinaryTreePostorderTraversal.java
@@ -25,3 +25,50 @@ The number of the nodes in the tree is in the range [0, 100].
 
 Follow up: Recursive solution is trivial, could you do it iteratively?
 */
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) { 
+        this.val = val; 
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public void traversal(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            traversal(root.left, res);
+            traversal(root.right, res);
+            res.add(root.val);
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        traversal(root, res);
+        return res;
+    }
+}
+
+public class BinaryTreePostorderTraversal {
+    public static void main(String[] args) {
+        // Example usage
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        Solution solution = new Solution();
+        List<Integer> result = solution.postorderTraversal(root);
+        System.out.println(result); // Output: [3, 2, 1]
+    }
+}
