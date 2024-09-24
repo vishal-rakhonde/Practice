566f30f625670395ae067ab1fc874d3e91b7b2b8
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-09-24 22:36:07 +0530

    Medium

---
+++ LeetCode/Find the Length of the Longest Common Prefix/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        HashMap<Integer, Integer> prefix = new HashMap<>();
+        
+        for (int val : arr1) {
+            int num = val;
+            while (num > 0) {
+                prefix.put(num, prefix.getOrDefault(num, 0) + 1);
+                num /= 10;
+            }
+        }
+
+        int maxLen = 0;
+
+        for (int val : arr2) {
+            int num = val;
+            int len = (int) Math.log10(num) + 1;
+
+            while (num > 0) {
+                if (prefix.containsKey(num)) {
+                    break;
+                }
+                num /= 10;
+                len--;
+            }
+
+            maxLen = Math.max(maxLen, len);
+        }
+
+        return maxLen;
+    }
+}
