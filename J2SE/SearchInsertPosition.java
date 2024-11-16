68f24c254245079b8d9e2ef55cf8b8a7d28acf5b
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-11-16 19:26:53 +0530

    Easy

--- J2SE/SearchInsertPosition.java
+++ J2SE/SearchInsertPosition.java
@@ -6,8 +6,7 @@ public class SearchInsertPosition {
         int left = 0, right = nums.length - 1;
 
         while (left <= right) {
-            int mid = left + (right - left) / 2; // Avoid potential overflow
-
+            int mid = left + (right - left) / 2; 
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
@@ -25,11 +24,9 @@ public class SearchInsertPosition {
 
         int n = sc.nextInt();
         int[] nums = new int[n];
-
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
-
         int target = sc.nextInt();
 
         System.out.println(searchInsertPosition(nums, target));
