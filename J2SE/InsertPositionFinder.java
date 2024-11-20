03084317f4f503314553fae95aa46207e07e56af
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-11-20 23:16:55 +0530

    Easy

--- J2SE/InsertPositionFinder.java
+++ J2SE/InsertPositionFinder.java
@@ -11,7 +11,6 @@ class InsertPositionFinder {
         }
         return left;
     }
-
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int n = sc.nextInt();
