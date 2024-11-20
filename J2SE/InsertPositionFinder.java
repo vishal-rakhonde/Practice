cbec21940b75c4d9322c81c09b657bdde3a7f188
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-11-20 23:15:27 +0530

    Easy

--- J2SE/InsertPositionFinder.java
+++ J2SE/InsertPositionFinder.java
@@ -1,13 +1,13 @@
 import java.util.*;
+
 class InsertPositionFinder {
     public static int searchInsertPosition(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        
-        while (left <= right) {
+        int left = 0, right = nums.length;
+
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid;
         }
         return left;
     }
@@ -15,7 +15,10 @@ class InsertPositionFinder {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int n = sc.nextInt();
-            int[] nums1 = Arrays.stream(new int[n]).map(i -> sc.nextInt()).toArray();
+            int[] nums1 = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums1[i] = sc.nextInt();
+            }
             int key = sc.nextInt();
             System.out.println(searchInsertPosition(nums1, key));
         }
