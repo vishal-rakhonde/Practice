a30c4267465c2eae5d91b2b92fc6e963b5f04311
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-11-25 11:15:27 +0530

    Easy

---
+++ J2SE/SwapWithoutThirdVariable.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SwapWithoutThirdVariable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Before Swap: num1 = " + num1 + ", num2 = " + num2);
+
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        System.out.println("After Swap: num1 = " + num1 + ", num2 = " + num2);
+    }
+}
