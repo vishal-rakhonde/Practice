a9f7824bbdc3d08ffb2b4fbf8df7c0371d58adfd
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-11-28 23:07:08 +0530

    Easy

--- J2SE/OpenLockers.java
+++ J2SE/OpenLockers.java
@@ -1,8 +1,9 @@
 public class OpenLockers {
     public static void main(String[] args) {
-        int n = 100;
+        int n = 100;// Total number of lockers
         System.out.println("Lockers that remain open:");
         for (int i = 1; i * i <= n; i++) {
+			// Perfect squares remain open
             System.out.print((i * i) + " ");
         }
     }
