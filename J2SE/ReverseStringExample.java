78419876dbdd8cb916a3c7eac6b8815e04c1217f
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-12-02 00:18:48 +0530

    Easy

--- J2SE/ReverseStringExample.java
+++ J2SE/ReverseStringExample.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class ReverseStringExample {
     public static void main(String[] args) {
-        String original = "Hello, World!";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string to reverse: ");
+        String original = scanner.nextLine();
+
         StringBuilder stringBuilder = new StringBuilder(original);
         String reversed = stringBuilder.reverse().toString();
+
         System.out.println("Original String: " + original);
         System.out.println("Reversed String: " + reversed);
+
+        scanner.close();
     }
 }
