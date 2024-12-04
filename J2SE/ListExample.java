122b6385570c917301d4437c853f1272650c4b47
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-12-04 21:23:05 +0530

    collection

--- J2SE/ListExample.java
+++ J2SE/ListExample.java
@@ -1,21 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ListExample {
     public static void main(String[] args) {
         List<String> fruits = new ArrayList<>();
-        fruits.add("Apple");
-        fruits.add("Banana");
-        fruits.add("Cherry");
-        fruits.add(1, "Mango");
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("First fruit: " + fruits.get(0));
+        System.out.println("Enter the number of fruits:");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Enter the names of the fruits:");
+        for (int i = 0; i < n; i++) {
+            String fruit = sc.nextLine();
+            fruits.add(fruit);
+        }
+
         System.out.println("Fruits in the list:");
         for (String fruit : fruits) {
             System.out.println(fruit);
         }
 
-        fruits.remove("Banana");
+        System.out.println("Enter the name of the fruit to remove:");
+        String fruitToRemove = sc.nextLine();
+        fruits.remove(fruitToRemove);
+
         System.out.println("After removal: " + fruits);
+        sc.close();
     }
 }
