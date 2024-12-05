cbfbb9805df2fd8ace61ed35787979ebaeacc955
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-12-06 00:35:57 +0530

    stack

--- J2SE/StackExample.java
+++ J2SE/StackExample.java
@@ -1,19 +1,31 @@
+import java.util.Scanner;
 import java.util.Stack;
 
 public class StackExample {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Using 'sc' as the reference
         Stack<Integer> stack = new Stack<>();
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
+
+        System.out.print("Enter the number of elements to push onto the stack: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            stack.push(sc.nextInt());
+        }
         System.out.println("Stack after pushing elements: " + stack);
 
-        System.out.println("Top element: " + stack.peek());
+        if (!stack.isEmpty()) {
+            System.out.println("Top element: " + stack.peek());
 
-        int poppedElement = stack.pop();
-        System.out.println("Popped element: " + poppedElement);
-        System.out.println("Stack after popping an element: " + stack);
+            int poppedElement = stack.pop();
+            System.out.println("Popped element: " + poppedElement);
+            System.out.println("Stack after popping an element: " + stack);
+        } else {
+            System.out.println("Stack is empty.");
+        }
 
         System.out.println("Is stack empty? " + stack.isEmpty());
+        sc.close();
     }
 }
