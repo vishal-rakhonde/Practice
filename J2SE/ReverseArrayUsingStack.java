c019964286ef45450d64747bcf684474de838195
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-12-09 23:02:03 +0530

    Stack

--- J2SE/ReverseArrayUsingStack.java
+++ J2SE/ReverseArrayUsingStack.java
@@ -1,15 +1,14 @@
+import java.util.Scanner;
 import java.util.Stack;
 
 public class ReverseArrayUsingStack {
     public static void reverseArray(int[] array) {
         Stack<Integer> stack = new Stack<>();
 
-        // Push elements of the array onto the stack
         for (int element : array) {
             stack.push(element);
         }
 
-        // Pop elements from the stack back into the array
         int index = 0;
         while (!stack.isEmpty()) {
             array[index++] = stack.pop();
@@ -17,7 +16,17 @@ public class ReverseArrayUsingStack {
     }
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+        System.out.println("Enter " + n + " elements of the array:");
+
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
 
         System.out.println("Original Array:");
         for (int num : array) {
