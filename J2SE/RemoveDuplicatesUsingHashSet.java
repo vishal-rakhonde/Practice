31e87d63307e4fe71ed552937a16cc64ec7628c7
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-12-13 00:34:49 +0530

    collection

--- J2SE/RemoveDuplicatesUsingHashSet.java
+++ J2SE/RemoveDuplicatesUsingHashSet.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class RemoveDuplicatesUsingHashSet {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // Renamed to 'sc'
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
         StringBuilder result = new StringBuilder();
