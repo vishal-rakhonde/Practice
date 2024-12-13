833956febace93b1ccdec6f5443ce9d6cb378fed
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-12-13 23:26:05 +0530

    collection

--- J2SE/RemoveDuplicatesUsingHashSet.java
+++ J2SE/RemoveDuplicatesUsingHashSet.java
@@ -14,7 +14,6 @@ public class RemoveDuplicatesUsingHashSet {
                 result.append(c);
             }
         }
-
         System.out.println("String after removing duplicates: " + result.toString());
     }
 }
