869b6f1ff6f858193c8a5c40489035ed7658eaa3
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-12-14 13:51:22 +0530

    tree

---
+++ J2SE/BinaryTree.java
@@ -0,0 +1,28 @@
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.println("Binary tree is created.");
+    }
+}
