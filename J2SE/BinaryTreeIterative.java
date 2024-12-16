f08accad37c234227ba7f4931cd90e4538f8d4e6
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-12-16 12:02:46 +0530

    tree

---
+++ J2SE/BinaryTreeIterative.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        left = right = null;
+    }
+}
+
+public class BinaryTreeIterative {
+    TreeNode root;
+
+    public void preorderTraversal(TreeNode root) {
+        if (root == null) return;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            System.out.print(node.val + " ");
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeIterative tree = new BinaryTreeIterative();
+        tree.root = new TreeNode(1);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(3);
+        tree.root.left.left = new TreeNode(4);
+        tree.root.left.right = new TreeNode(5);
+
+        tree.preorderTraversal(tree.root);
+    }
+}
