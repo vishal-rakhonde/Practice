1af23800f28243e49df20991af6fd00445b19df4
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-01-08 22:29:46 +0530

    LinkedList

---
+++ ADS/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in the LinkedList:");
+        int n = sc.nextInt();
+
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            int element = sc.nextInt();
+            list.add(element);
+        }
+
+        System.out.println("Original LinkedList: " + list);
+
+        Collections.reverse(list);
+
+        System.out.println("Reversed LinkedList: " + list);
+
+        sc.close();
+    }
+}
