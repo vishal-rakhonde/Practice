cbdd61fa897f93ec4b3401fc31970c17ed125438
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-01-09 18:19:05 +0530

    Interface

---
+++ J2SE/InterfaceExample.java
@@ -0,0 +1,20 @@
+interface Animal {
+	void sound();
+	void sleep();
+}
+class Dog implements Animal{
+	public void sound(){
+		System.out.println("dog barks");
+	}
+	public void sleep(){
+		System.out.println("Dog Sleeps");
+	}
+}
+public class InterfaceExample {
+	public static void main(String args[]){
+		Dog dog=new Dog();
+		dog.sound();
+		dog.sleep();
+	}
+}
+	
