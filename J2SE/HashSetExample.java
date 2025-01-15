314af7f5e06da2ccf216dc61e52f992de8deb27f
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-01-15 18:26:06 +0530

    HashSet

---
+++ J2SE/HashSetExample.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+public class HashSetExample{
+	public static void main(String[] args){
+		HashSet<String> set = new HashSet<>();
+		
+		set.add("Apple");
+		set.add("Banana");
+		set.add("cherry");
+		
+		System.out.println("HashSet: "+set);
+	}
+}
