c8210266996013170af6215543fb62dee7522f31
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-01-16 00:09:16 +0530

    HashSet

--- J2SE/RemoveDuplicatesFromArray.java
+++ J2SE/RemoveDuplicatesFromArray.java
@@ -5,21 +5,16 @@ import java.util.Scanner;
 public class RemoveDuplicatesFromArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter the size of the array: ");
         int size = sc.nextInt();
-
         int[] array = new int[size];
         System.out.println("Enter the elements of the array:");
         for (int i = 0; i < size; i++) {
             array[i] = sc.nextInt();
         }
-
         System.out.println("Original Array: " + Arrays.toString(array));
-        
         int[] result = removeDuplicates(array);
         System.out.println("Array after removing duplicates: " + Arrays.toString(result));
-        
         sc.close();
     }
 
