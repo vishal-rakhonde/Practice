6174c3bd14d4ef1bc790c6b20893ee39675cb271
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-01-24 22:12:12 +0530

    ArrayList

---
+++ J2SE/ArrayListExample.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+class ArrayListExample {
+	public static void main(String args[]){
+		ArrayList<String> list=new ArrayList<>();
+		list.add("vishal");
+		list.add("Ajay");
+		list.add("Vaibhav");
+		System.out.println(list);
+	}
+}
+		
