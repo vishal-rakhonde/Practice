8d1b2d90dfa731c84ff0bb15eda89e36e320240c
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-01-28 22:23:52 +0530

    Easy

---
+++ J2SE/StringContainsExample.java
@@ -0,0 +1,8 @@
+public class StringContainsExample {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        boolean contains = str.contains("ell");
+        System.out.println("Does the string contain 'ell'? " + contains);
+    }
+}
+
