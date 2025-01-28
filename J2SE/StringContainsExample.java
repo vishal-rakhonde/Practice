d8cec1a583d1ad988b1c8129015e0412fdf49865
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-01-28 22:25:41 +0530

    Easy

--- J2SE/StringContainsExample.java
+++ J2SE/StringContainsExample.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class StringContainsExample {
     public static void main(String[] args) {
-        String str = "Hello World";
-        boolean contains = str.contains("ell");
-        System.out.println("Does the string contain 'ell'? " + contains);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        
+        System.out.print("Enter the substring to check: ");
+        String substring = sc.nextLine();
+        
+        boolean contains = str.contains(substring);
+        System.out.println("Does the string contain '" + substring + "'? " + contains);
+        
+        sc.close();
     }
 }
-
