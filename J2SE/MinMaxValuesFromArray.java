22adc707b1252a81773cb93ab43ef86f5b23ccb0
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-02-03 23:02:09 +0530

    Easy

--- J2SE/MinMaxValuesFromArray.java
+++ J2SE/MinMaxValuesFromArray.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 class MinMaxValuesFromArray {
-	public static void main(String[] args){
-		int[] arr = {40,55,65,43,10,85,22,655};
-		int min= arr[0];
-		int max = arr[0];
-		for(int i=1;i<arr.length;i++) {
-			if(arr[i] < min) min = arr[i];
-			if(arr[i] > max) max = arr[i];
-		}
-		System.out.println("Minimum : "+ min);
-		System.out.println("Maximum : "+ max);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
+        }
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+
+        sc.close();
+    }
 }
