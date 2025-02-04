b6558374d54b311581fe96e3deb749e823fecde0
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-02-04 15:44:06 +0530

    String

--- J2SE/StringManipulation.java
+++ J2SE/StringManipulation.java
@@ -1,9 +1,11 @@
 /* Given a string with a mix of small and capital letters, write a program to change all
 small letters to capital and all capital letters to small. */
-
+import java.util.Scanner;
 class StringManipulation {
 	public static void main(String[] args){
-		String str="AbCDef";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter string");
+		String str = sc.nextLine();
 		StringBuilder modified=new StringBuilder();
 		
 		for(int i=0;i<str.length();i++){
@@ -14,8 +16,8 @@ class StringManipulation {
 			else if(Character.isUpperCase(ch)){
 				modified.append(Character.toLowerCase(ch));
 			}
-			
 		}
 		System.out.println("Modified String is : "+modified );
+		sc.close();
 	}
 }					
