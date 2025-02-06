04c45cfc8b350e1cd92966ee508e52e6358c8e8c
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-02-06 21:58:41 +0530

    Easy

--- J2SE/DuplicateElementsFromArrayUsingHashSet.java
+++ J2SE/DuplicateElementsFromArrayUsingHashSet.java
@@ -1,8 +1,17 @@
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Scanner;
 
 class DuplicateElementsFromArrayUsingHashSet {
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of elements in array : ");
+		int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
 		int[] arr={10,60,55,88,55,66,77,40,10};
 		Set<Integer> s = new HashSet <>();
 		System.out.println("Duplicate Elements from array are : ");
@@ -11,4 +20,5 @@ class DuplicateElementsFromArrayUsingHashSet {
 				System.out.print(no+" ");
 			}
 		}
-}}
+	}
+}
