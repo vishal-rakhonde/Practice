90356bc1ae4ea0a786e6aa43d0246b1a9e9d4941
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-02-08 23:22:12 +0530

    Easy

---
+++ J2SE/ZerosAndOnes.java
@@ -0,0 +1,18 @@
+class ZerosAndOnes {
+    public static void main(String[] args) {
+        int arr[] = {0, 1, 1, 0, 0, 0, 1, 1};
+        int low = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                int temp = arr[i];
+                arr[i] = arr[low];
+                arr[low] = temp;
+                low++; 
+            }
+        }
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
