d55a21eaeec235b82a5d976b01751a464b6c8283
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-02-13 20:14:05 +0530

    Easy

---
+++ J2SE/StarSquare.java
@@ -0,0 +1,16 @@
+class StarSquare {
+	public static void main(String[] args){
+		int n=10;
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n;j++){
+				if(i==1 || i==n || j==1 || j==n){
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
