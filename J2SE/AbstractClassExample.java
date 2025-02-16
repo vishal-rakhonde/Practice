8eb880814590001d72430b1b1339b43e30ae5fc6
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-02-16 23:20:14 +0530

    Easy

--- J2SE/AbstractClassExample.java
+++ J2SE/AbstractClassExample.java
@@ -1,17 +1,20 @@
-abstract class AbstractClassExample {
-	abstract void print();
-	public void display(){
-		System.out.println("In display method");
+abstract class Animal {
+	public abstract void makeSound();
+	public void eat(){
+		System.out.println("This Animal is Eating");
 	}
 }
-class Demo extends AbstractClassExample {
-	@Override 
-	void print(){
-		System.out.println("In print method");
+class Dog extends Animals {
+	@Override
+	public void makeSound(){
+		System.out.println("bark");
 	}
+}
+public class AbstractClassExample {
 	public static void main(String[] args){
-		Demo d=new Demo();
-		d.print();
-		d.display();
+		Dog d= new Dog();
+		d.makeSound();
+		d.eat();
 	}
 }
+		
