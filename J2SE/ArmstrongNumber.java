0836d98463c31aae5532a82a359b4299c8a8e4f3
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-02-21 22:30:49 +0530

    Easy

--- J2SE/ArmstrongNumber.java
+++ J2SE/ArmstrongNumber.java
@@ -1,33 +1,18 @@
-public class ArmstrongNumber {
-
-	public static void main(String[] args) {
-		// we find firstly length on the using while loop
-		int no=153;
-		int t1=no;			//t1 is using because of the original no is not to be changed
-		int leng=0;			//leng is using because of the to finding the length
-		while(t1!=0) {
-			leng=leng+1;
-			t1=t1/10;
+class  ArmstrongNumber {
+	public static void main(String[] args){
+		int num=253;
+		int res;
+		int temp= num;
+		int sum=0;
+		while(num > 0){
+			res= num%10;
+			num= num/10;
+			sum= sum+(res * res * res);
 		}
-		//we are finding the armstrong number 
-		int t2=no; 			//t2 is using because of the original no is not to be changed
-		int arm=0;
-		int rem;
-		while(t2!=0) {
-			int mul=1;
-		rem=t2%10;
-		for(int i=1;i<=leng;i++) {
-			mul=mul*rem;
+		if(sum == temp){
+			System.out.println("Number is armstrong");
 		}
-		arm=arm+mul;
-		t2=t2/10;
-		}
-		if(no==arm) {
-			System.out.println(no+" is a Armstrong Number");
-		}else {
-			System.out.println(no+" is not a Armstrong Number");
-		}
-
+		else
+			System.out.println("Number is not armstrong");
 	}
-
 }
