16d8c22ead557e84f438e0c136132c703bb17926
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-02-22 23:00:11 +0530

    Easy

---
+++ J2SE/DiamondPattern.java
@@ -0,0 +1,21 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 7; // add odd size
+        int mid = n / 2;
+
+        for (int i = 0; i < n; i++) {
+            int stars = 2 * (mid - Math.abs(mid - i)) + 1; 
+            int spaces = (n - stars) / 2; 
+
+            printChars(' ', spaces);
+            printChars('*', stars);
+            System.out.println();
+        }
+    }
+
+    private static void printChars(char ch, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.print(ch);
+        }
+    }
+}
