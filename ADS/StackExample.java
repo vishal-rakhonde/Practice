eb1ebf49e9af7f2d6f33b0406e1b94fc20a6e5e1
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-03-03 21:55:08 +0530

    Easy

--- ADS/StackExample.java
+++ ADS/StackExample.java
@@ -1,21 +1,33 @@
 import java.util.*;
+
 class StackExample {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Stack<Integer> stack = new Stack<>();
         
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
+        System.out.println("Enter the number of elements to push:");
+        int n = sc.nextInt();
+        
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            stack.push(scanner.nextInt());
+        }
         
         System.out.println("Stack: " + stack);
         
-        int poppedElement = stack.pop();
-        System.out.println("Popped Element: " + poppedElement);
-        System.out.println("Stack after pop: " + stack);
+        if (!stack.isEmpty()) {
+            int poppedElement = stack.pop();
+            System.out.println("Popped Element: " + poppedElement);
+            System.out.println("Stack after pop: " + stack);
+        }
         
-        int topElement = stack.peek();
-        System.out.println("Top Element: " + topElement);
+        if (!stack.isEmpty()) {
+            int topElement = stack.peek();
+            System.out.println("Top Element: " + topElement);
+        }
         
         System.out.println("Is stack empty? " + stack.isEmpty());
+        
+        sc.close();
     }
 }
