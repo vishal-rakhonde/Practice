2a921147baa3f85d750efd0e701c63cd1be85898
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-03-09 22:55:07 +0530

    Medium

---
+++ ADS/SearchInsertPosition.java
@@ -0,0 +1,32 @@
+public class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+        System.out.println(searchInsert(nums, target));
+
+        target = 2;
+        System.out.println(searchInsert(nums, target));
+
+        target = 7;
+        System.out.println(searchInsert(nums, target));
+
+        target = 0;
+        System.out.println(searchInsert(nums, target));
+    }
+}
