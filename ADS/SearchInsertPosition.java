3f65670086747902cb69416df0289e1c5cce0aa9
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-03-11 23:02:05 +0530

    Medium

--- ADS/SearchInsertPosition.java
+++ ADS/SearchInsertPosition.java
@@ -1,7 +1,6 @@
 public class SearchInsertPosition {
     public static int searchInsert(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
