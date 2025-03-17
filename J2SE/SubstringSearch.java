b93c88baf09062f38efd7fdb4f73ae53c3ab9886
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-03-17 23:14:46 +0530

    Easy

---
+++ J2SE/SubstringSearch.java
@@ -0,0 +1,17 @@
+public class SubstringSearch {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.startsWith(needle, i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        SubstringSearch solution = new SubstringSearch();
+        System.out.println(solution.strStr("sadbutsad", "sad")); 
+        System.out.println(solution.strStr("leetcode", "leeto"));
+    }
+}
