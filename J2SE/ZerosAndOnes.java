3b366b94184c55df83e54dee351168ae741c96ca
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2025-05-14 20:20:39 +0530

    Update ZerosAndOnes.java

--- J2SE/ZerosAndOnes.java
+++ J2SE/ZerosAndOnes.java
@@ -2,7 +2,6 @@ class ZerosAndOnes {
     public static void main(String[] args) {
         int arr[] = {0, 1, 1, 0, 0, 0, 1, 1};
         int low = 0;
-
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
                 int temp = arr[i];
